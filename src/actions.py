2df358a9f307b7914c0d53033654edb14776adf6
hmanickam13 <hmanickam13@gmail.com>
2023-09-29 12:34:56 -0500

    t

--- src/actions.py
+++ src/actions.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the Python scripts you want to run
-scripts_to_run = ["src/01-rssfeedscraper.py"] #, "src/02-rawfeedsfoldercreator.py", "src/03-GetArticles.py", "src/04-SummarizeArticle.py", "src/05-GetMarkdown.py"]
+scripts_to_run = ["src/02-rawfeedsfoldercreator.py"]#, "src/03-GetArticles.py", "src/04-SummarizeArticle.py", "src/05-GetMarkdown.py"]
 
 # Loop through and run each script
 for script in scripts_to_run:
@@ -9,3 +9,4 @@ for script in scripts_to_run:
         subprocess.run(["python", script], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running {script}: {e}")
+
