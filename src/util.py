48e3efd17fcda58aa9243822368e0ba2d1df442f
hmanickam13 <hmanickam13@gmail.com>
2023-10-21 16:31:30 -0500

    t

--- src/util.py
+++ src/util.py
@@ -7,10 +7,10 @@ def is_github_action():
 def get_filepath(filename):
     """Return the appropriate filepath depending on the environment."""
     if is_github_action():
-        if filename == 'status.json':
-            base_path = os.environ['GITHUB_WORKSPACE']
-        else:
-            base_path = os.path.join(os.environ['GITHUB_WORKSPACE'], 'src')
+        # if filename == 'status.json':
+        #     base_path = os.environ['GITHUB_WORKSPACE']
+        # else:
+        base_path = os.path.join(os.environ['GITHUB_WORKSPACE'], 'src')
     else:
         base_path = ''
     return os.path.join(base_path, filename)
