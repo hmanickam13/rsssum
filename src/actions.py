34f81863cba07e684d518e39ddd4eebc1e3563b0
hmanickam13 <hmanickam13@gmail.com>
2023-11-12 21:38:20 -0600

    debug

--- src/actions.py
+++ src/actions.py
@@ -6,10 +6,10 @@ import os
 print(os.getcwd())
 
 # Define the Python scripts you want to run
-# scripts_to_run = ["src/01-RSSFeedScraper.py",
-#                   "src/02-GetArticles.py",
-#                   "src/03-SummarizeArticles.py",
-scripts_to_run = ["src/04-GenerateHTML.py"]
+scripts_to_run = ["src/01-RSSFeedScraper.py",
+                  "src/02-GetArticles.py",
+                  "src/03-SummarizeArticles.py",
+                  "src/04-GenerateHTML.py"]
 
 # Loop through and run each script
 for script in scripts_to_run:
